0a56ea913e579d93163317bad6552a37585f51f2
ssompally <ssompally@gmail.com>
2024-04-25 18:33:26 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import LandingPage from './pages/LandingPage'; // Import your LandingPage component
+import BuyPage from './pages/BuyPage'; // Import your BuyPage component
+import SellPage from './pages/SellPage'; // Import your SellPage component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <Link to="/" className="App-link">Home</Link> | {" "}
+            <Link to="/buy" className="App-link">Buy</Link> | {" "}
+            <Link to="/sell" className="App-link">Sell</Link>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/buy" element={<BuyPage />} />
+            <Route path="/sell" element={<SellPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/BuyPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function BuyPage() {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.header}>Buy</h1>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    textAlign: 'center', 
+    padding: '50px', 
+    color: '#333', 
+    backgroundColor: '#f4f4f4', 
+    borderRadius: '8px', 
+    margin: '20px auto', 
+    maxWidth: '600px',
+  },
+  header: {
+    color: '#0264d6', // Set the color of the header text
+  }
+};
+
+export default BuyPage;
